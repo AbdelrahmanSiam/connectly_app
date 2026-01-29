dc20907541e0b2d73f8dbd1572d767f2f269fd42
Abdulrahman <syamabdo382@gmail.com>
2026-01-29 04:44:54 +0200

    create user cubit methods

--- lib/features/profile/presentation/manager/cubit/user_cubit.dart
+++ lib/features/profile/presentation/manager/cubit/user_cubit.dart
@@ -6,4 +6,41 @@ part 'user_state.dart';
 
 class UserCubit extends Cubit<UserState> {
   UserCubit() : super(UserInitialState());
+
+  void setUser(UserModel userModel){
+    emit(UserSuccessState(userModel: userModel));
+  }
+
+  void clearUser(){
+    emit(UserInitialState());
+  }
+  
+  void updateUser({ String ? name , String ? bio , String ? profilePictureUrl , bool ? isOnline}){
+    final currentState  = state ;
+    if(currentState is UserSuccessState){
+      final updatedState = currentState.copyWith(
+        name: name,
+        bio: bio,
+        profilePictureUrl: profilePictureUrl,
+        isOnline: isOnline,
+      );
+      
+      emit(updatedState);
+    } else {
+      print('❌ UserCubit: Cannot update - no user loaded');
+    }
+  }
+
+  UserModel? get currentUser {
+    final currentState = state;
+    
+    if (currentState is UserSuccessState) {
+      return currentState.userModel;
+    }
+    
+    return null;
+  } // to get current user
+
+    bool get isUserLoaded => state is UserSuccessState; // check there user or not
+
 }

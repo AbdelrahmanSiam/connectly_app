d38f16dfd79457db8c3ce7e8edc4f07100cf0a2f
Abdulrahman <syamabdo382@gmail.com>
2026-02-03 03:03:39 +0200

    add loadChats method to cubit

--- lib/features/home/presentation/manager/chats_cubit/chats_cubit.dart
+++ lib/features/home/presentation/manager/chats_cubit/chats_cubit.dart
@@ -1,9 +1,26 @@
 import 'package:bloc/bloc.dart';
 import 'package:connectly_app/features/home/data/model/chat_list_tile.dart';
+import 'package:connectly_app/features/home/data/repo/home_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'chats_state.dart';
 
 class ChatsCubit extends Cubit<ChatsState> {
-  ChatsCubit() : super(ChatsInitialState());
+  ChatsCubit(this.homeRepo, {required this.currentUserId}) : super(ChatsInitialState());
+
+  final HomeRepo homeRepo;
+  final String currentUserId;
+
+  void loadChats(){
+    emit(ChatsLoadingState());
+    homeRepo.getChats(currentUserId).listen((allChatsList)async{
+      final List <ChatListTileModel> chatsListTile = []; // we add this model that contains two models , because screen need data from these two different models 
+      for(final chat in allChatsList){
+        final otherUserId = chat.users.firstWhere((id) => id != currentUserId); // get uid of the other person who chat me from list users in chat model
+        final user = await homeRepo.getUserById(otherUserId); // get UserModel of other user id to show his photo , name , online state
+        chatsListTile.add(ChatListTileModel(chatModel: chat, userModel: user));
+      }
+      emit(ChatsSuccessState(chats: chatsListTile));
+    });
+  }
 }

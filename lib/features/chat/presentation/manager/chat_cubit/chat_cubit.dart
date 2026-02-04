9d241a585527a4cccef9faa2b8884130b7970c90
Abdulrahman <syamabdo382@gmail.com>
2026-02-05 01:42:35 +0200

    add chat cubit methods

--- lib/features/chat/presentation/manager/chat_cubit/chat_cubit.dart
+++ lib/features/chat/presentation/manager/chat_cubit/chat_cubit.dart
@@ -1,9 +1,34 @@
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:connectly_app/features/chat/data/model/message_model.dart';
+import 'package:connectly_app/features/chat/data/repo/chat_repo.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:meta/meta.dart';
 
 part 'chat_state.dart';
 
 class ChatCubit extends Cubit<ChatState> {
-  ChatCubit() : super(ChatInitialState());
+  ChatCubit(this.chatRepo) : super(ChatInitialState());
+  final ChatRepo chatRepo;
+  final String myId = FirebaseAuth.instance.currentUser!.uid;
+
+  void loadMessages(String chatId) {
+    emit(ChatLoadingState());
+    chatRepo.getChatMessages(chatId: chatId).listen((messageList) {
+      emit(ChatSuccesState(messageList: messageList));
+    }, onError: (error) {
+      emit(ChatFailureState(errMessage: error.toString()));
+    });
+  }
+  // only text need to receive it from user
+  Future<void> sendMessage(String chatId, String text) async {
+    if (text.trim().isEmpty) return;
+    final messageModel = MessageModel(
+        messageId: "",
+        text: text,
+        senderId: myId,
+        isSeen: false,
+        createdAt: DateTime.now());
+    await chatRepo.sendMessage(chatId: chatId, messageModel: messageModel);
+  }
 }

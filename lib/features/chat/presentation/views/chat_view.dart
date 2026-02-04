62f6273d54207d73d594666d029fe0fa1a7c3c7e
Abdulrahman <syamabdo382@gmail.com>
2026-02-05 01:46:21 +0200

    provide chat cubit

--- lib/features/chat/presentation/views/chat_view.dart
+++ lib/features/chat/presentation/views/chat_view.dart
@@ -1,5 +1,8 @@
+import 'package:connectly_app/core/utils/service_locator.dart';
+import 'package:connectly_app/features/chat/presentation/manager/chat_cubit/chat_cubit.dart';
 import 'package:connectly_app/features/chat/presentation/views/widgets/chat_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ChatView extends StatelessWidget {
   const ChatView({super.key, required this.chatId});
@@ -7,7 +10,12 @@ class ChatView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ChatViewBody(chatId: chatId,),
+      body: BlocProvider(
+        create: (context) => getIt<ChatCubit>()..loadMessages(chatId),
+        child: ChatViewBody(
+          chatId: chatId,
+        ),
+      ),
     );
   }
 }

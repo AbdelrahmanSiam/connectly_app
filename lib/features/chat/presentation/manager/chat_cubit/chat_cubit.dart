47f115a8f799cfd140775e9ea3b7d606dfeb95aa
Abdulrahman <syamabdo382@gmail.com>
2026-02-05 01:31:41 +0200

    add chat cubit states

--- lib/features/chat/presentation/manager/chat_cubit/chat_cubit.dart
+++ lib/features/chat/presentation/manager/chat_cubit/chat_cubit.dart
@@ -1,8 +1,9 @@
 import 'package:bloc/bloc.dart';
+import 'package:connectly_app/features/chat/data/model/message_model.dart';
 import 'package:meta/meta.dart';
 
 part 'chat_state.dart';
 
 class ChatCubit extends Cubit<ChatState> {
-  ChatCubit() : super(ChatInitial());
+  ChatCubit() : super(ChatInitialState());
 }
--- lib/features/chat/presentation/manager/chat_cubit/chat_state.dart
+++ lib/features/chat/presentation/manager/chat_cubit/chat_state.dart
@@ -3,4 +3,15 @@ part of 'chat_cubit.dart';
 @immutable
 sealed class ChatState {}
 
-final class ChatInitial extends ChatState {}
+final class ChatInitialState extends ChatState {}
+final class ChatLoadingState extends ChatState {}
+final class ChatSuccesState extends ChatState {
+  final List<MessageModel> messageList;
+
+  ChatSuccesState({required this.messageList});
+}
+final class ChatFailureState extends ChatState {
+  final String errMessage;
+
+  ChatFailureState({required this.errMessage});
+}

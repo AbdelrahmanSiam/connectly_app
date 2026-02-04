d70cd794bb3b852bfaf6fc2578e95beb13835eac
Abdulrahman <syamabdo382@gmail.com>
2026-02-05 01:02:35 +0200

    create abstract chat repo methods

---
+++ lib/features/chat/data/repo/chat_repo.dart
@@ -0,0 +1,6 @@
+import 'package:connectly_app/features/chat/data/model/message_model.dart';
+
+abstract class ChatRepo {
+  Stream<List<MessageModel>> getChatMessages({required String chatId});
+  Future<void> sendMessage({required String chatId , required MessageModel messageModel});
+}

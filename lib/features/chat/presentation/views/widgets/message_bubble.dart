a884d2634f9129d9113fdcb437ac929403dbb7a8
Abdulrahman <syamabdo382@gmail.com>
2026-02-05 02:11:13 +0200

    add message bubble

---
+++ lib/features/chat/presentation/views/widgets/message_bubble.dart
@@ -0,0 +1,46 @@
+import 'package:connectly_app/core/utils/app_colors.dart';
+import 'package:connectly_app/features/chat/data/model/message_model.dart';
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  final MessageModel messageModel;
+  final bool isMe;
+
+  const MessageBubble({
+    super.key,
+    required this.messageModel,
+    required this.isMe,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 14),
+        constraints: BoxConstraints(
+          maxWidth: MediaQuery.of(context).size.width * 0.75, // to be responsive
+        ),
+        decoration: BoxDecoration(
+          color: isMe ? AppColors.primary : Colors.grey.shade200,
+          borderRadius: BorderRadius.only(
+            topLeft: const Radius.circular(18),
+            topRight: const Radius.circular(18),
+            bottomLeft:
+                isMe ? const Radius.circular(18) : const Radius.circular(4),
+            bottomRight:
+                isMe ? const Radius.circular(4) : const Radius.circular(18),
+          ),
+        ),
+        child: Text(
+          messageModel.text,
+          style: TextStyle(
+            color: isMe ? Colors.white : Colors.black,
+            fontSize: 16,
+          ),
+        ),
+      ),
+    );
+  }
+}

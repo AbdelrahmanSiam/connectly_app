f44832e59335d713e90f81faaf9b988994a8baf4
Abdulrahman <syamabdo382@gmail.com>
2026-02-07 08:31:37 +0200

    add method to repo

--- lib/features/profile/data/repo/user_repo.dart
+++ lib/features/profile/data/repo/user_repo.dart
@@ -1,3 +1,6 @@
+import 'package:connectly_app/features/profile/data/model/user_model.dart';
+
 abstract class UserRepo {
   Future<int> getChatsCount({required String userId});
+  Future<UserModel> getUserModelById(String userId); 
 }
--- lib/features/profile/data/repo/user_repo_impl.dart
+++ lib/features/profile/data/repo/user_repo_impl.dart
@@ -1,3 +1,4 @@
+import 'package:connectly_app/features/profile/data/model/user_model.dart';
 import 'package:connectly_app/features/profile/data/repo/user_repo.dart';
 import 'package:connectly_app/features/profile/data/service/user_service.dart';
 
@@ -8,6 +9,11 @@ class UserRepoImpl implements UserRepo{
   @override
   Future<int> getChatsCount({required String userId}) async{
    return await userService.getChatsCount(userId);
+  }
+
+  @override
+  Future<UserModel> getUserModelById(String userId) async {
+    return await userService.getUserModelById(userId);
   }
 
 }

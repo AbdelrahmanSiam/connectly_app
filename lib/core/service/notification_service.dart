7d8256f4ecab7aaa09a9b70259f07e5fcb007635
Abdulrahman <syamabdo382@gmail.com>
2026-02-09 00:54:55 +0200

    add requestPermission to service

--- lib/core/service/notification_service.dart
+++ lib/core/service/notification_service.dart
@@ -1,3 +1,13 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
 class NotificationService {
-  
+  final FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  Future<void> requestPermission() async {
+    await messaging.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+  }
 }
